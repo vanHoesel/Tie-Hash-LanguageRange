b80da8c837899c7f93a93a58d86950ea170f3197
Theo van Hoesel <Th.J.v.Hoesel@THEMA-MEDIA.nl>
2015-08-17 21:34:53 +0100

    Initial Commit after some nice demo’s

---
+++ example/some_hash.pl
@@ -0,0 +1,30 @@
+#use warnings;
+use strict;
+
+use FindBin;
+use lib "$FindBin::Bin/../lib";
+
+use DDP;
+
+use Hash::LanguageTag 'en-GB, nl, en, it, en-US';
+
+my %Hello_World;
+
+tie %Hello_World, 'Hash::LanguageTag', qw/fr nl/;
+
+my $lang = undef;
+
+$Hello_World{$lang} = "Guten Tag";
+$Hello_World{nl} = "Hallo Wereld";
+
+my $line = $Hello_World{$lang};
+p $line;
+
+my $whoohoo = $Hello_World{'en, nl, de'};
+p $whoohoo;
+
+printf("Lang: '%s' => %s\n", $_, $Hello_World{$_}) foreach keys %Hello_World;
+
+# print %Hello_World->DEFAULTS;
+
+__END__
